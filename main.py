b0732a39e3637899ae4415f5a2eeeefd9fc9efd4
Padykk <pmit2570@gmail.com>
2026-09-14 19:56:26 +0200

    adding delta time and Circleshape.py

--- main.py
+++ main.py
@@ -15,14 +15,27 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     # získá se instance GUI okna
 
+    clock_object = pygame.time.Clock()
+    dt = 0.0 #delta time
+
     while True: #nekonečná loop (game loop)
         log_state()
 
         for event in pygame.event.get():
-            pass
+            if event.type == pygame.QUIT:
+                return
+            #kontroluje eventy, pokud uživatel zmáčkl X
+            # hru to zavře
 
         screen.fill("black")
         pygame.display.flip() #refresne screen
+
+        dt = clock_object.tick(60) / 1000
+        # .tick() pozastaví program na potřebnou dobu
+        # 60 je max FPS, které chci (můžu zvolit jaké chci)
+        # zároveň to vrátí číslo, konkrétně ms, které uběhly
+        # od minulého volání .tick()
+        # dělím 1000 protože to chci v sekundách, ne ms
 
 if __name__ == "__main__":
     main()
